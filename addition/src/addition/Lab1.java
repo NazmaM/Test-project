113ddcb34632abf325b86247ccfc560fd6dcc1c2
DELL <DELL@DESKTOP-06MP6T8>
2021-07-08 02:09:57 +0530

    added class lab1.java

---
+++ addition/src/addition/Lab1.java
@@ -0,0 +1,15 @@
+package addition;
+
+public class Lab1 {
+	public static void main(String[] args) {
+		System.out.println("Enter two numbers");
+		int first = 98;
+		int second = 102;
+		System.out.println(first + " " + second);
+		// add two numbers
+		int sum = first + second;
+		System.out.println("The sum is: " + sum);
+		}
+		}
+
+
